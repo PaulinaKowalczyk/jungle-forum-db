ee6df0f761327dae833aafd3a362e59670db3553
Macbook <macbook@MacBook-Pro-Macbook-2.local>
2019-05-06 17:55:32 +0200

    first commit

---
+++ migrations/20190506173644_user.js
@@ -0,0 +1,15 @@
+
+exports.up = (knex, Promise) => {
+  return knex.schema.createTable('users', (table) => {
+      table.increments();
+      table.text('display_name').notNullable();
+      table.text('email').notNullable();
+      table.text('google_id').notNullable();
+      table.boolean('banned').notNullable().default(false);
+
+  })
+};
+
+exports.down = (knex, Promise) => {
+  return knex.schema.dropTable('users');
+};
